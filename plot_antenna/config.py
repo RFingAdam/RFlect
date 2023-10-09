a9addd768952cd24038d51471dd58386cabc68f6
Maritime Blockchain <112358679+MaritimeBlockchain@users.noreply.github.com>
2023-10-09 13:21:04 -0500

    Started Active Scan Implementation - Parsing, Calculations, and Azimuth Power Pattern Cuts vs. Phi for various value of Theta

--- plot_antenna/config.py
+++ plot_antenna/config.py
@@ -4,11 +4,15 @@ Configuration file for the RFlect application.
 This module contains constants and configuration values used throughout the application.
 """
 
-#3D Plotting Interpolation Resolution
+# 3D Plotting Interpolation Resolution for viewing plots (lower for better performance)
 PHI_RESOLUTION = 120 #default 360 = 1deg spacing
 THETA_RESOLUTION = 60 #default 180 - 1deg spacing
 
-#Set Min/Max for optional 2D polar plots on passive scans - set in settings
+# TODO 3D Plotting Interpolation Resolution for Saving plots (higher for better resolution in documentation)
+PHI_RESOLUTION_Save = 360 #default 360 = 1deg spacing
+THETA_RESOLUTION_Save = 180 #default 180 - 1deg spacing
+
+# Set Min/Max for optional 2D polar plots on passive scans - set in settings
 polar_dB_max = 5.0
 polar_dB_min = -20.0
 
@@ -21,6 +25,6 @@ BUTTON_COLOR = "#3A3A3A"
 HOVER_COLOR = "#4A4A4A"
 ACCENT_GREEN_COLOR = "#4CAF50"
 
-    # Fonts
+# Fonts
 HEADER_FONT = ("Arial", 14, "bold")
 LABEL_FONT = ("Arial", 12)
